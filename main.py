2e6303b8e93d257786bf279efec72dbdaf43054e
andyafter <andyafter@gmail.com>
2018-11-25 14:07:26 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import h2o
+import time
+import seaborn
+import itertools
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from h2o.estimators.glm import H2OGeneralizedLinearEstimator
+from h2o.estimators.gbm import H2OGradientBoostingEstimator
+from h2o.estimators.random_forest import H2ORandomForestEstimator
